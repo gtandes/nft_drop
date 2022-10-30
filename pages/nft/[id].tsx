588e301ee7e1fa479f1408a63146ccf04f6b3cf2
Greg Thomas Andes <58809832+gtandes@users.noreply.github.com>
2022-10-31 04:53:11 +0800

    day 3 finished

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,70 +1,80 @@
 import React from 'react';
 import { ConnectWallet, useAddress } from '@thirdweb-dev/react';
+import { GetServerSideProps } from 'next';
+import { imageBuilder, sanityClient } from '../../sanity';
+import { Collection } from '../../typings';
+import Link from 'next/link';
 
-const NFTDropPage = () => {
+interface Props {
+	collection: Collection;
+}
+
+const NFTDropPage = ({ collection }: Props) => {
 	const address = useAddress();
 	return (
-		<div className='flex h-screen flex-col lg:grid lg:grid-cols-10'>
+		<div className='flex flex-col h-screen lg:grid lg:grid-cols-10'>
 			{/* left side */}
 			<div className='bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4'>
 				<div className='flex flex-col items-center justify-center py-2 lg:min-h-screen'>
-					<div className='rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600 p-2'>
+					<div className='p-2 rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600'>
 						<img
-							src='https://links.papareact.com/8sg'
+							src={imageBuilder(collection.previewImage).url()}
 							alt=''
-							className='w-44 rounded-xl object-cover lg:h-96 lg:w-72'
+							className='object-cover w-44 rounded-xl lg:h-96 lg:w-72'
 						/>
 					</div>
 
-					<div className='space-y-2 p-5 text-center'>
-						<h1 className='text-4xl font-bold text-white'>GT Apes</h1>
-						<h2 className='text-xl text-gray-300'>
-							A collection of GT Apes who live and breathe in React JS!
-						</h2>
+					<div className='p-5 space-y-2 text-center'>
+						<h1 className='text-4xl font-bold text-white'>
+							{collection.nftCollectionName}
+						</h1>
+						<h2 className='text-xl text-gray-300'>{collection.description}</h2>
 					</div>
 				</div>
 			</div>
 
 			{/* right */}
-			<div className='flex flex-1 flex-col p-12 lg:col-span-6'>
+			<div className='flex flex-col flex-1 p-12 lg:col-span-6'>
 				{/* header */}
 				<header className='flex items-center justify-between'>
-					<h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>
-						The{' '}
-						<span className='font-extrabold underline decoration-pink-600/50'>
-							GTA
-						</span>{' '}
-						NFT Marketplace
-					</h1>
+					<Link href={'/'}>
+						<h1 className='text-xl cursor-pointer w-52 font-extralight sm:w-80'>
+							The{' '}
+							<span className='font-extrabold underline decoration-pink-600/50'>
+								GTA
+							</span>{' '}
+							NFT Marketplace
+						</h1>
+					</Link>
 
 					<div
 					// type='button'
 					// title='Connect Wallet'
-					// className='rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base'
+					// className='px-4 py-2 text-xs font-bold text-white rounded-full bg-rose-400 lg:px-5 lg:py-3 lg:text-base'
 					>
-						<ConnectWallet className='rounded-full bg-rose-400' />
+						<ConnectWallet />
 					</div>
 				</header>
 
 				<hr className='my-2 border' />
 
 				{/* content */}
-				<div className='mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0'>
+				<div className='flex flex-col items-center flex-1 mt-10 space-y-6 text-center lg:justify-center lg:space-y-0'>
 					<img
-						src='https://links.papareact.com/bdy'
-						className='w-80 object-cover pb-10 lg:h-40'
+						src={imageBuilder(collection.mainImage).url()}
+						className='object-cover pb-10 w-80 lg:h-40'
 						alt=''
 					/>
 
 					<h1 className='text-3xl font-bold lg:text-5xl lg:font-extrabold'>
-						The GT Ape NFT Drop
+						{collection.title}
 					</h1>
 
 					<p className='pt-2 text-xl text-green-500'>13/21 NFTs claimed</p>
 				</div>
 
 				{/* mint button */}
-				<button className='mt-10 h-16 w-full rounded-full bg-red-600 font-bold text-white'>
+				<button className='w-full h-16 mt-10 font-bold text-white bg-red-600 rounded-full'>
 					Mint NFT (0.01 ETH)
 				</button>
 			</div>
@@ -73,3 +83,38 @@ const NFTDropPage = () => {
 };
 
 export default NFTDropPage;
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+	const query = `*[_type == "collection" && slug.current == $id][0]{
+  _id,
+  title,
+  address,
+  description,
+  nftCollectionName,
+  mainImage{
+      asset
+  },
+  previewImage{
+      asset
+  },
+  slug{
+      current
+  },
+  creator-> {
+    _id,
+    name,
+    address,
+    slug{
+       current
+    },
+  },
+ }`;
+
+	const collection = await sanityClient.fetch(query, { id: params?.id });
+
+	if (!collection) {
+		return { notFound: true };
+	}
+
+	return { props: { collection } };
+};
